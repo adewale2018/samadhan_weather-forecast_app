7c89c59ae8eb11fad1a50dba0a9c40c30437a9bd
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-05-06 14:55:43 +0100

    installations and setting up of the project directory

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     {/* <a href="/" class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a> */}
+    </div>
+  );
+}
+
+export default App;
