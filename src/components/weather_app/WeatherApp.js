ec0a6e5100c34afadbf7709fc49dfef1f7c1ef54
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-05-16 21:46:35 +0100

    display the fetched data on the UI

--- src/components/weather_app/WeatherApp.js
+++ src/components/weather_app/WeatherApp.js
@@ -13,7 +13,7 @@ const API = {
 const WeatherApp = () => {
   const [timezone, setTimeZone] = useState("");
   const [current, setCurrent] = useState({});
-  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState([]);
   const [daily, setDaily] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [coordinates, setCoordinates] = useState({});
@@ -46,6 +46,7 @@ const WeatherApp = () => {
     setTimeZone(timezone);
     setCurrent(current);
     setDaily(daily);
+    setWeather(current.weather[0]);
   };
 
   const searchCity = (city) => {
@@ -60,14 +61,20 @@ const WeatherApp = () => {
   }, []);
 
   const timeConverter = (dt) => new Date(dt * 1000).toUTCString();
+  const timeConverter2 = (dt) => new Date(dt * 1000).toLocaleTimeString();
+  const dateConverter = (dt) => new Date(dt * 1000).toLocaleDateString();
 
   const { lat, lng } = coordinates;
-  const { dt, temp, humidity } = current;
+  const { dt, temp, humidity, sunrise, sunset } = current;
+  const { description, icon } = weather;
+  // const {dt, rain, humidity, temp, weather } = daily;
+  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  // const iconUrl2 = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
   if (isLoading) {
     return (
       <div>
-        <h4 className="teal-text">Loading...</h4>
+        <h4 className='teal-text'>Loading...</h4>
         <div className='preloader-wrapper big active'>
           <div className='spinner-layer spinner-blue-only'>
             <div className='circle-clipper left'>
@@ -88,10 +95,9 @@ const WeatherApp = () => {
     <div>
       <SearchBar searchCity={searchCity} />
       <section>
-        <div className='divider'></div>
+        <div className='divider' />
         <div className='container center teal-text'>
           <h4 className='center'>{timezone.toUpperCase()}</h4>
-          <span style={{fontSize: '1.3rem', fontStyle: "italic"}}>{timeConverter(dt)}</span>
           <div className='row center'>
             <div className='col s12 m3 center'>
               <div className='card'>
@@ -107,6 +113,12 @@ const WeatherApp = () => {
                     <ul className='collection'>
                       <li className='collection-item'>Latitude: {lat}</li>
                       <li className='collection-item'>Longitude: {lng}</li>
+                      <li className='collection-item'>
+                        Sunrise: {timeConverter2(sunrise)}
+                      </li>
+                      <li className='collection-item'>
+                        Sunset: {timeConverter2(sunset)}
+                      </li>
                     </ul>
                   </span>
                 </div>
@@ -122,7 +134,80 @@ const WeatherApp = () => {
                 </div>
               </div>
             </div>
-            <div className='col s12 m9 center'></div>
+            <div className='col s12 m9 center'>
+              <h5 style={{ fontSize: "1.3rem", fontStyle: "italic" }}>
+                {timeConverter(dt)}
+              </h5>
+              <div>
+                <div className='col s12 m4'>
+                  <h6>TEMPERATURE</h6>
+                  <p className='card-panel card-panel-1'>{temp}&deg;C</p>
+                </div>
+                <div className='col s12 m4'>
+                  <h6>HUMIDITY</h6>
+                  <p className='card-panel card-panel-1'>{humidity}&#37;</p>
+                </div>
+                <div className='col s12 m4'>
+                  <h6>WEATHER</h6>
+                  <p className='card-panel'>
+                    {description}
+                    <img
+                      src={iconUrl}
+                      alt={description}
+                      className='weather-icon'
+                    />
+                  </p>
+                </div>
+              </div>
+              <hr />
+              <div>
+                <h4 className="center ">DAILY</h4>
+                {daily
+                  .slice(0, 3)
+                  .map(({ dt, rain, humidity, temp, weather }) => (
+                    <div className='col s12 m4' key={dt}>
+                      <h6>{dateConverter(dt)}</h6>
+
+                      <ul className='collection with-header'>
+                        <li className='collection-item'>
+                          Precipitation: {rain}mm
+                        </li>
+                        <li className='collection-item'>
+                          Humidity: {humidity}&#37;
+                        </li>
+                        <li className='collection-header'>
+                          <h6>TEMPERATURE</h6>
+                          <ul className='collection'>
+                            <li className='collection-item'>
+                              Morning: {temp.morn}&deg;C
+                            </li>
+                            <li className='collection-item'>
+                              Evening: {temp.eve}&deg;C
+                            </li>
+                            <li className='collection-item'>
+                              Night: {temp.night}&deg;C
+                            </li>
+                          </ul>
+                        </li>
+                        <li className='collection-header'>
+                          <h6>WEATHER</h6>
+                          <ul className='collection'>
+                            <li className='collection-item'>
+                              {weather[0].description}
+                            </li>
+                            <li className='collection-item'>
+                              <img
+                                src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+                                alt={weather[0].description}
+                              />
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div>
+                  ))}
+              </div>
+            </div>
           </div>
         </div>
       </section>
